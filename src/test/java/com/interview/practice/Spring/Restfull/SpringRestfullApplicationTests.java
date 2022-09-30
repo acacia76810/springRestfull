8cb48e752ac4c3f4434c54d3c12fc658ae8e946f
Amit Mangal <amitmangal17@gmail.com>
2022-09-30 23:49:32 +0530

    ORIGINAL COMMIT

---
+++ src/test/java/com/interview/practice/Spring/Restfull/SpringRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.practice.Spring.Restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
